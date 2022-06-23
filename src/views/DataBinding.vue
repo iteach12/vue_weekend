38966de439e07cd61fd3f509eaf720426d33c2ea
Park Sang Jun <iteach12@gmail.com>
2022-06-23 22:52:09 +0900

    return

--- src/views/DataBinding.vue
+++ src/views/DataBinding.vue
@@ -2,17 +2,17 @@
   <h1>문자열 데이터 바인딩</h1>
   <h2>Hello, {{ title }}</h2>
   <p>중괄호 두개를 이용해서 데이터 삽입</p>
-  <hr width="50%" color="gray" />
+  <hr width="70%" color="gray" />
   <h1>raw HTML 데이터 바인딩</h1>
   <h2>{{ htmlstring }}</h2>
   <h2 v-html="htmlstring"></h2>
   <p>v-html 디렉티브(지시문)을 사용하여 입력.</p>
-  <hr width="50%" color="gray" />
+  <hr width="70%" color="gray" />
   <h1>Form 입력 데이터 바인딩</h1>
   <input type="text" v-model="valueModel" />
 
   <p>v-model 디렉티브(지시문)을 사용하여 입력.</p>
-  <hr width="50%" color="gray" />
+  <hr width="70%" color="gray" />
 </template>
 <script>
 export default {
