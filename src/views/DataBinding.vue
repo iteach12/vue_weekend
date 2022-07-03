ae0ddf5e214bfa1a13fbacbf27f5433abadcd27c
iteach12 <iteach12@gmail.com>
2022-07-03 22:57:59 +0900

    vue study

--- src/views/DataBinding.vue
+++ src/views/DataBinding.vue
@@ -21,10 +21,10 @@
   <input type="text" v-model="valueModel" />
   <h3>input type=number</h3>
   <p>이 경우 문자열이 아닌 숫자열로 관리됨.</p>
-  <input type="number" v-model="numberModel">
+  <input type="number" v-model.number="numberModel">
   <h3>textarea v-model 사용</h3>
   <textarea v-model="message"></textarea>
-  <h3>Select v-mode 사용</h3>
+  <h3>Select v-model 사용</h3>
   <select v-model="school">
     <option value="02">성북초</option>
     <option value="03">횡성초</option>
@@ -32,6 +32,36 @@
   </select>
   </div>
   <hr width="70%" color="gray" />
+  <h3>checkbox => checked 속성 사용</h3>
+  <p>체크박스에서는 v-model이 체크박스의 value 속성이 아닌 checked속성을 사용하기 때문에, value속성에 데이터 바인딩을 하려면 v-model이 아닌 v-bind:value를 사용해야 함.</p>
+  <div>
+    <label><input type="checkbox" value="서울" v-model="checked"> 서울</label>
+    <label><input type="checkbox" value="울산" v-model="checked"> 울산</label>
+    <label><input type="checkbox" value="부산" v-model="checked"> 부산</label>
+    <br>
+    <span>체크한 지역 : {{checked}}</span>
+  </div>
+  <hr>
+  <h2>리스트 렌더링(v-for)</h2>
+  <table>
+    <thead>
+      <tr>
+      <th>제품명</th>
+      <th>가격</th>
+      <th>카테고리</th>
+      <th>배송료</th>
+    </tr>
+    </thead>
+    <tbody>
+      <tr :key="i" v-for="(product, i) in productList">
+        <td>{{product.product_name}}</td>
+        <td>{{product.price}}</td>
+        <td>{{product.category}}</td>
+        <td>{{product.delivery_price}}</td>
+      </tr>
+    </tbody>
+    
+  </table>
 </template>
 <script>
 export default {
@@ -43,7 +73,26 @@ export default {
       numberModel:3,
       message:'이것은 텍스트 에어리어 입니다. v-model을 사용합니다.',
       school:"064",
+      checked:[],
+      productList:[
+        {"product_name":"무선마우스", "price":12000, "category":"노트북/태블릿", "delivery_price":5000},
+        {"product_name":"마우스패드", "price":18000, "category":"악세서리", "delivery_price":3000},
+        {"product_name":"기계식키보드", "price":22000, "category":"키보드", "delivery_price":2000},
+        {"product_name":"의자", "price":42000, "category":"가구", "delivery_price":4000},
+      ],
     };
   },
 };
 </script>
+<style>
+table{
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width:100%;
+}
+td, th{
+  border:1px solid #dddddd;
+  text-align:left;
+  padding:8px;
+}
+</style>
